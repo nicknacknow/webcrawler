a5fd029ad7f2616d0e32ccf641b0a8fc87b69e2e
unknown <16gilbertn@harveygs.kent.sch.uk>
2022-06-27 16:08:22 +0100

    got something working, formdata is weird

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,20 @@
 window.addEventListener("DOMContentLoaded", function(event){
     document.querySelector("#AmazonLinkForm").addEventListener("submit", function(event)
     {
-        event.preventDefault();
         var formData = new FormData(document.querySelector("#AmazonLinkForm"));
-        var options = {body: formData, method: "POST", cache: 'no-cache'};
-        console.log(Array.from(formData));
-        
-        fetch("/submit-form", options).then(function(response){
-            return response;
-        }).then(function(data){
-            console.log(data);
-        }).catch(function(err){console.log(err);})
-        //event.currentTarget.submit(event);
+        event.preventDefault();
+
+        (async () => {
+            const response = await fetch("/submit-form", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify((formData.values()))
+            });
+            const content = await response.json();
+            console.log("reply : " + content);
+        })();
     });
 })
